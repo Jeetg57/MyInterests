d8c610ecdfd2f4c0a216d126e9e911db8fb96171
jeetg57 <jeetg57@gmail.com>
2020-03-15 17:56:29 +0300

    Achievent Add

---
+++ app/src/main/java/com/jeetg57/myinterests/AddAchievement.java
@@ -0,0 +1,14 @@
+package com.jeetg57.myinterests;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddAchievement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_achievement);
+    }
+}
